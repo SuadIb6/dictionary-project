fdd4a2352b5a070d91e4166f2f89849f85f23030
SuadIb6 <suadibrahim1020@gmail.com>
2023-12-05 22:47:55 +0000

    added dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,23 +3,23 @@ import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  let [word, setKeyword] = useState("");
+  const [word, setKeyword] = useState("");
 
-  function handleResponse(response) {
-    console.log(response.data[0]);
-  }
+  const handleResponse = (response) => {
+    console.log(response.data.meanings[0]);
+  };
 
-  function search(event) {
+  const search = (event) => {
     event.preventDefault();
-
-    // documentation: https://dictionaryapi.dev/e
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key={b385d498dfa178bfc115dfc214tao005}`;
+    alert(`Searching for ${word} definition`);
+    const apiKey = "b385d498dfa178bfc115dfc214tao005";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
-  }
+  };
 
-  function handleKeywordChange(event) {
+  const handleKeywordChange = (event) => {
     setKeyword(event.target.value);
-  }
+  };
 
   return (
     <div className="Dictionary">
